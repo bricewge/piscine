88b548e10f454108059c5908f5871e9aa4f75b01
Brice WAEGENEIRE <bricewge@gmail.com>
2016-07-07 07:56:33 +0200

    ex04 pass norminette

--- ex04/ft_print_comb.c
+++ ex04/ft_print_comb.c
@@ -6,11 +6,25 @@
 /*   By: bwaegene <brice.wge@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/06 17:20:04 by bwaegene          #+#    #+#             */
-/*   Updated: 2016/07/06 23:30:59 by bwaegene         ###   ########.fr       */
+/*   Updated: 2016/07/07 07:52:36 by bwaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
+
+void	print(int one, int two, int three)
+{
+	ft_putchar(one);
+	ft_putchar(two);
+	ft_putchar(three);
+	if ((one == '7') && (two == '8') && (three == '9'))
+		ft_putchar('.');
+	else
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
 
 void	ft_print_comb(void)
 {
@@ -24,16 +38,7 @@ void	ft_print_comb(void)
 			{
 				if ((triple[2] != triple[1]) && (triple[1] != triple[0]))
 				{
-					ft_putchar(triple[0]);
-					ft_putchar(triple[1]);
-					ft_putchar(triple[2]);
-					if ((triple[0] == '7') && (triple[1] == '8') && (triple[2] == '9'))
-						ft_putchar('.');
-					else
-					{
-						ft_putchar(',');
-						ft_putchar(' ');
-					}
+					print(triple[0], triple[1], triple[2]);
 				}
 				triple[2]++;
 			}
